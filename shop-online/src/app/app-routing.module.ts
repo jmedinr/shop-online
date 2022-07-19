5a24179a021d2ce0a113bf6aced3facf786da1fd
Juan Medina Rojas <juan.medina@sofka.local>
2022-07-19 11:41:37 -0500

    add --- list cards

--- shop-online/src/app/app-routing.module.ts
+++ shop-online/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule,Routes} from '@angular/router';
+import { ListComponent } from './components/list/list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'view', pathMatch: 'full' },
+  { path: 'view', component: ListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule { }
